9bee4a2448129a7d2e76928165c5292e8f3740c4
kb-math <kamil.bulinski@gmail.com>
2019-06-30 11:46:11 +0930

    hello OOP :)

--- project_7/VM_translator.py
+++ project_7/VM_translator.py
@@ -20,6 +20,12 @@ with open("asm_templates/push_fixed_segment_i.template") as f:
 with open("asm_templates/pop_fixed_segment_i.template") as f:
 	pop_fixed_segment_i_template = f.read()
 
+with open("asm_templates/push_static.template") as f:
+	push_static_template = f.read()
+
+with open("asm_templates/pop_static.template") as f:
+	pop_static_template = f.read()
+
 with open("asm_templates/arithmetic.template") as f:
 	arithmetic_template = f.read()
 
@@ -40,140 +46,141 @@ OPERATORS = {"not": "!", "neg": "-"}
 # i.e., < is lt, = is eq, > is gt
 COMPARISONS = {"lt": "JLT", "eq": "JEQ", "gt": "JGT"}
 
-LABEL_COUNTER = 0
+class AssemblyCodeGenerator(object):
+	def __init__(self):
+		self._LABEL_COUNTER = 0
+		self._current_vm_file = ""
 
-#put <value> into RAM[<address>]
-def set_value_code(address, value):
-	code_str = str(set_value_template)
-	code_str = code_str.replace("<address>", str(address))
-	code_str = code_str.replace("<value>", str(value))
+	#put <value> into RAM[<address>]
+	def set_value_code(self, address, value):
+		code_str = str(set_value_template)
+		code_str = code_str.replace("<address>", str(address))
+		code_str = code_str.replace("<value>", str(value))
 
-	return code_str
+		return code_str
 
-def push_constant_code(i):
-	code_str = str(push_constant_template)
-	code_str = code_str.replace("<i>", str(i))
-
-	return code_str
-
-def push_segment_i_code(segment, i):
-	#quite annoying that temp and static don't have a pointer like LCL, ARG etc.
-	#requires a different code
-	if segment in FIXED_BASES.keys():
-		code_str = str(push_fixed_segment_i_template)
-		segment_base = FIXED_BASES[segment]
-		code_str = code_str.replace("<fixed_segment>", str(segment))
-		code_str = code_str.replace("<fixed_segment_base>", str(segment_base))
+	def push_constant_code(self, i):
+		code_str = str(push_constant_template)
 		code_str = code_str.replace("<i>", str(i))
-	elif segment in BASE_ADDRESS_LOCATIONS.keys():
-		code_str = str(push_segment_i_template)
-		segment_base_address_location = BASE_ADDRESS_LOCATIONS[segment]
-		code_str = code_str.replace("<segment>", str(segment))
-		code_str = code_str.replace("<segment_base_address_location>", str(segment_base_address_location))
-		code_str = code_str.replace("<i>", str(i))
-	else:
-		raise Exception ("unknown segment", segment) 
 
-	return code_str
+		return code_str
 
-#TODO: code duplication with pop and push? (apart from template name really)
-def pop_segment_i_code(segment, i):
-	#quite annoying that temp and static don't have a pointer like LCL, ARG etc.
-	#requires a different code
-	if segment in FIXED_BASES.keys():
-		code_str = str(pop_fixed_segment_i_template)
-		segment_base = FIXED_BASES[segment]
-		code_str = code_str.replace("<fixed_segment>", str(segment))
-		code_str = code_str.replace("<fixed_segment_base>", str(segment_base))
-		code_str = code_str.replace("<i>", str(i))
-	elif segment in BASE_ADDRESS_LOCATIONS.keys():
-		code_str = str(pop_segment_i_template)
-		segment_base_address_location = BASE_ADDRESS_LOCATIONS[segment]
-		code_str = code_str.replace("<segment>", str(segment))
-		code_str = code_str.replace("<segment_base_address_location>", str(segment_base_address_location))
-		code_str = code_str.replace("<i>", str(i))
-	else:
-		raise Exception ("unknown segment", segment)
+	def push_segment_i_code(self, segment, i):
+		#quite annoying that temp and static don't have a pointer like LCL, ARG etc.
+		#requires a different code
+		if segment in FIXED_BASES.keys():
+			code_str = str(push_fixed_segment_i_template)
+			segment_base = FIXED_BASES[segment]
+			code_str = code_str.replace("<fixed_segment>", str(segment))
+			code_str = code_str.replace("<fixed_segment_base>", str(segment_base))
+			code_str = code_str.replace("<i>", str(i))
+		elif segment in BASE_ADDRESS_LOCATIONS.keys():
+			code_str = str(push_segment_i_template)
+			segment_base_address_location = BASE_ADDRESS_LOCATIONS[segment]
+			code_str = code_str.replace("<segment>", str(segment))
+			code_str = code_str.replace("<segment_base_address_location>", str(segment_base_address_location))
+			code_str = code_str.replace("<i>", str(i))
+		else:
+			raise Exception ("unknown segment", segment) 
 
-	return code_str
+		return code_str
 
-def arithmetic_code(operation):
-	code_str = str(arithmetic_template)
-	code_str = code_str.replace("<operation>", str(operation))
-	operation_symbol = OPERATIONS[operation]
-	code_str = code_str.replace("<operation_symbol>", str(operation_symbol))
+	#TODO: code duplication with pop and push? (apart from template name really)
+	def pop_segment_i_code(self, segment, i):
+		#quite annoying that temp and static don't have a pointer like LCL, ARG etc.
+		#requires a different code
+		if segment in FIXED_BASES.keys():
+			code_str = str(pop_fixed_segment_i_template)
+			segment_base = FIXED_BASES[segment]
+			code_str = code_str.replace("<fixed_segment>", str(segment))
+			code_str = code_str.replace("<fixed_segment_base>", str(segment_base))
+			code_str = code_str.replace("<i>", str(i))
+		elif segment in BASE_ADDRESS_LOCATIONS.keys():
+			code_str = str(pop_segment_i_template)
+			segment_base_address_location = BASE_ADDRESS_LOCATIONS[segment]
+			code_str = code_str.replace("<segment>", str(segment))
+			code_str = code_str.replace("<segment_base_address_location>", str(segment_base_address_location))
+			code_str = code_str.replace("<i>", str(i))
+		else:
+			raise Exception ("unknown segment", segment)
 
-	return code_str
+		return code_str
 
-def operator_code(operator):
-	code_str = str(operator_template)
-	code_str = code_str.replace("<operator>", str(operator))
-	operator_symbol = OPERATORS[operator]
-	code_str = code_str.replace("<operator_symbol>", str(operator_symbol))
+	def arithmetic_code(self, operation):
+		code_str = str(arithmetic_template)
+		code_str = code_str.replace("<operation>", str(operation))
+		operation_symbol = OPERATIONS[operation]
+		code_str = code_str.replace("<operation_symbol>", str(operation_symbol))
 
-	return code_str
+		return code_str
 
-def comparison_code(comparison):
-	global LABEL_COUNTER
+	def operator_code(self, operator):
+		code_str = str(operator_template)
+		code_str = code_str.replace("<operator>", str(operator))
+		operator_symbol = OPERATORS[operator]
+		code_str = code_str.replace("<operator_symbol>", str(operator_symbol))
 
-	code_str = str(comparison_template)
-	code_str = code_str.replace("<comparison>", str(comparison))
-	comparison_jump_symbol = COMPARISONS[comparison]
-	code_str = code_str.replace("<comparison_jump_symbol>", str(comparison_jump_symbol))
+		return code_str
 
-	# replace <label> with string label_<LABEL_COUNTER>_label and increment to avoid
-	#clashes between labels
-	label = "jump_here_if_comparison_is_true"
-	label_indexed = "label_" + str(LABEL_COUNTER) + "_" + label
-	code_str = code_str.replace(label, label_indexed)
-	LABEL_COUNTER += 1
+	def comparison_code(self, comparison):
+		code_str = str(comparison_template)
+		code_str = code_str.replace("<comparison>", str(comparison))
+		comparison_jump_symbol = COMPARISONS[comparison]
+		code_str = code_str.replace("<comparison_jump_symbol>", str(comparison_jump_symbol))
 
-	label = "end_comparison_block"
-	label_indexed = "label_" + str(LABEL_COUNTER) + "_" + label
-	code_str = code_str.replace(label, label_indexed)
-	LABEL_COUNTER += 1
+		# replace <label> with string label_<self._LABEL_COUNTER>_label and increment to avoid
+		#clashes between labels
+		label = "jump_here_if_comparison_is_true"
+		label_indexed = "label_" + str(self._LABEL_COUNTER) + "_" + label
+		code_str = code_str.replace(label, label_indexed)
+		self._LABEL_COUNTER += 1
 
-	return code_str
+		label = "end_comparison_block"
+		label_indexed = "label_" + str(self._LABEL_COUNTER) + "_" + label
+		code_str = code_str.replace(label, label_indexed)
+		self._LABEL_COUNTER += 1
+
+		return code_str
 
 
-def read_vm_code_line(code_line):
-	code_split = [x for x in str(code_line).split() if x != ""]
+	def read_vm_code_line(self, code_line):
+		code_split = [x for x in str(code_line).split() if x != ""]
 
-	try:
-		#empy line
-		if not code_split:
-			return ""
+		try:
+			#empy line
+			if not code_split:
+				return ""
 
-		if code_split[0][0] == "/":
-			if code_split[0][1] == "/":
-				#it is a comment, should we keep or remove it?
-				return code_line
+			if code_split[0][0] == "/":
+				if code_split[0][1] == "/":
+					#it is a comment, should we keep or remove it?
+					return code_line
 
-		first_keyword = code_split[0]
+			first_keyword = code_split[0]
 
-		if first_keyword == "push":
-			second_keyword = code_split[1]
-			if second_keyword == "constant":
-				return push_constant_code(code_split[2])
-			else:
-				#second keyword must be a segment
-				return push_segment_i_code(second_keyword, code_split[2])
+			if first_keyword == "push":
+				second_keyword = code_split[1]
+				if second_keyword == "constant":
+					return self.push_constant_code(code_split[2])
+				else:
+					#second keyword must be a segment
+					return self.push_segment_i_code(second_keyword, code_split[2])
 
-		if first_keyword == "pop":
-			second_keyword = code_split[1]
-			return pop_segment_i_code(second_keyword, code_split[2])
-		if first_keyword in OPERATIONS.keys():
-			return arithmetic_code(first_keyword)
-		if first_keyword in OPERATORS.keys():
-			return operator_code(first_keyword)
-		if first_keyword in COMPARISONS.keys():
-			return comparison_code(first_keyword)
-	except Exception as e:
-		print str(e)
+			if first_keyword == "pop":
+				second_keyword = code_split[1]
+				return self.pop_segment_i_code(second_keyword, code_split[2])
+			if first_keyword in OPERATIONS.keys():
+				return self.arithmetic_code(first_keyword)
+			if first_keyword in OPERATORS.keys():
+				return self.operator_code(first_keyword)
+			if first_keyword in COMPARISONS.keys():
+				return self.comparison_code(first_keyword)
+		except Exception as e:
+			print str(e)
 
-	error_log = "failed to parse line " + code_line
+		error_log = "failed to parse line " + code_line
 
-	raise Exception(error_log)
+		raise Exception(error_log)
 
 if __name__ == '__main__':
 	
@@ -181,6 +188,8 @@ if __name__ == '__main__':
 	parser.add_argument('vm_code_file', help = ".vm file path to be translated to .asm")
 
 	args = parser.parse_args()
+
+	code_generator = AssemblyCodeGenerator()
 
 	source_code_fname = args.vm_code_file
 	with open(source_code_fname) as f:
@@ -191,7 +200,7 @@ if __name__ == '__main__':
 	with open(source_code_fname + ".asm", 'wa') as output_asm_file:
 		for code_line in source_code_lines:
 			try:
-				asm_code_line = read_vm_code_line(code_line)
+				asm_code_line = code_generator.read_vm_code_line(code_line)
 			except Exception as e:
 				print "failed at line", line_number
 				output_asm_file.write("COMPILATION_FAILED!")
